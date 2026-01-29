930c431540033f32ffef7746663df8dfef12ba5a
MiguelBravo <miguel_98-@hotmail.com>
2026-01-28 23:13:25 -0600

    add part 2 tests

---
+++ HammingDistance/Program.cs
@@ -0,0 +1,26 @@
+﻿// HAMMING distance
+
+string text1 = "patitosv";
+string text2 = "paratosa";
+
+int result = hammingDistance(text1, text2);
+
+Console.WriteLine(result);
+
+int hammingDistance(string textOne, string textTwo)
+{
+    int distance = 0;
+
+    if (textOne.Length != textTwo.Length)
+    {
+        throw new Exception("Different lengths");
+    }
+
+    for (int i = 0; i < textOne.Length; i++)
+    {
+        if (textOne[i] != textTwo[i])
+            distance++;
+    }
+
+    return distance;
+}
---
+++ MultipleValidations/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+Console.WriteLine("Enter a text to validate:");
+string input = Console.ReadLine();
+
+List<string> errors = Validate(input);
+
+if (errors.Any())
+{
+    string finalErrors = string.Join("\n - ", errors);
+    Console.WriteLine("\nErrors found:\n - " + finalErrors);
+}
+else
+{
+    Console.WriteLine("✔️ The string is valid");
+}
+
+List<string> Validate(string input)
+{
+    List<string> errors = new();
+
+    if (string.IsNullOrWhiteSpace(input))
+        errors.Add("The text cannot be empty.");
+
+    if (input.Length < 5)
+        errors.Add("It must have at least 5 characters.");
+
+    if (!input.Any(char.IsUpper))
+        errors.Add("It must include at least one uppercase letter.");
+
+    if (!input.Any(char.IsLower))
+        errors.Add("It must include at least one lowercase letter.");
+
+    if (!input.Any(char.IsDigit))
+        errors.Add("It must include at least one number.");
+
+    return errors;
+}
